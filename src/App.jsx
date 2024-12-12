1fe93035279d7fb7b6b9b96faee8a59850131db7
waaseyzulfiqar <wasey03112920792@gmail.com>
2024-12-12 22:37:14 +0500

    Login Page UI Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Login from "./components/Auth/Login";
+
 function App() {
-  return <></>;
+  return <div className="h-screen bg-slate-900 text-white">
+    <Login />
+  </div>;
 }
 
 export default App;
---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(email);
+    console.log(password);
+
+    setEmail('')
+    setPassword('')
+  }
+
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="border-2 border-red-800 p-12 rounded">
+        <h4 className="text-2xl font-semibold mb-24 tracking-tight">Log In</h4>
+        <form onSubmit={handleSubmit} className="text-center">
+          <div className="flex flex-col">
+            <input
+              required
+              className="px-6 py-2 text-xl w-80 bg-transparent border border-red-900 outline-none rounded-full mb-4"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              required
+              className="px-6 py-2 text-xl w-80 bg-transparent border border-red-900 outline-none rounded-full mb-4"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="flex justify-between mt-8">
+            <div className="flex gap-x-2">
+              <input
+                className="border border-red-900"
+                type="checkbox"
+                id="checkbox"
+              />
+              <label htmlFor="checkbox">Remember Me</label>
+            </div>
+            <p className="text-red-800 cursor-pointer">Forgot Password</p>
+          </div>
+
+          <div className="mt-10">
+            <input
+              className="bg-red-900 w-full py-2 rounded-full font-bold tracking-tight text-lg"
+              type="submit"
+              value="Log In"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
